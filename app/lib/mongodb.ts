efe6af950baf62b04e6b77498bfa020cf80ec522
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2025-11-12 12:03:09 +0100

    git add app/lib/mongodb.ts git commit -m "Fix: use const for clientPromise (ESLint prefer-const)" git push origin main

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
 declare global {
-  // Allow global caching for hot reload in dev
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
@@ -10,13 +9,13 @@ const uri = process.env.MONGODB_URI as string;
 const options = {};
 
 let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
 
 if (!global._mongoClientPromise) {
   client = new MongoClient(uri, options);
   global._mongoClientPromise = client.connect();
 }
 
-clientPromise = global._mongoClientPromise;
+const clientPromise = global._mongoClientPromise!;
 
 export default clientPromise;
+
