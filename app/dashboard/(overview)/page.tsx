995ba350fa454015e052ed4d682abd60ef135a9f
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2025-11-14 08:55:10 +0100

    done

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import CardWrapper from '@/app/ui/dashboard/cards'; // Correct import for CardWrapper
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -6,23 +6,22 @@ import { Suspense } from 'react';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
-  CardsSkeleton, // Import the skeleton for cards
+  CardsSkeleton,
 } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
-
 };
+
+// Force runtime rendering
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Dashboard
-      </h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
 
-      {/* Wrapped CardWrapper in Suspense */}
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
